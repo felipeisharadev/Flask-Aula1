ffe90cf1cb60f6f79b291db6b2ebedd49b6fbfad
felipeisharadev <felipeishara5@gmail.com>
2025-11-05 15:22:43 +0000

    rota info

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello, world !!</h1>"
+    return "<h1>Hello, world sss!!</h1>"
+
+@app.route('/info')
+def info():
+   modulo = "CSS"
+   aula = "1" 
+   return f"<h1>Modulo: {modulo}, Aula: {aula} </h1>"
 
